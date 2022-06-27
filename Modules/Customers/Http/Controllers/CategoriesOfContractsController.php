9a3876798a593ed4068d6c37354e7ba36996c445
mohammedobaid1234 <mhmd.obaid.18@gmail.com>
2022-06-27 10:23:32 +0300

    UP

--- Modules/Customers/Http/Controllers/CategoriesOfContractsController.php
+++ Modules/Customers/Http/Controllers/CategoriesOfContractsController.php
@@ -12,28 +12,28 @@ class CategoriesOfContractsController extends Controller{
 
     private $title = "أنوع العقود";
     private $model = \Modules\Customers\Entities\CategoriesOfContracts::class;
-    public function index(Request $request){
-        if(trim($request->search) == " " && trim($request->categories_of_contacts) == ""){
-            return ["data" => []];
-        }
+    // public function index(Request $request){
+    //     if(trim($request->search) == " " && trim($request->categories_of_contacts) == ""){
+    //         return ["data" => []];
+    //     }
 
-        $response = $this->model::select('*');
-        // $response->orderBy('created_at', 'DESC');
+    //     $response = $this->model::select('*');
+    //     // $response->orderBy('created_at', 'DESC');
 
-        if(is_numeric(trim($request->search))){
-            $response->where('categories_of_contacts', trim($request->search));
-        }elseif(trim($request->categories_of_contacts) !== ""){
-            $response->where('categories_of_contacts', trim($request->categories_of_contacts));
-        }else{
-            $response->whereFullNameLike($request->search);
-        }
+    //     if(is_numeric(trim($request->search))){
+    //         $response->where('categories_of_contacts', trim($request->search));
+    //     }elseif(trim($request->categories_of_contacts) !== ""){
+    //         $response->where('categories_of_contacts', trim($request->categories_of_contacts));
+    //     }else{
+    //         $response->whereFullNameLike($request->search);
+    //     }
 
-        if($request->has('page')){
-            return $response->paginate(20);
-        }
+    //     if($request->has('page')){
+    //         return $response->paginate(20);
+    //     }
 
-        return ["data" => $response->get()];
-    }
+    //     return ["data" => $response->get()];
+    // }
     public function manage(){
         \Auth::user()->authorize('customers_module_categories_of_contracts_manage');
 
